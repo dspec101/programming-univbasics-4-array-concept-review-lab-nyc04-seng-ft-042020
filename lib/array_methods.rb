5efcad5952e5c5db4844ca3519b3fcf0f1757a7d
David Spector <davidspector08@gmail.com>
2020-03-30 13:08:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,46 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+counter = 0
+while counter < array.size
+  if array[counter] == value_to_find 
+  return counter
+  end
+  counter += 1
 end
+ # iterate throught the array
+ # compare each element to value to find
+ # if you find a match, return that value
+end
+
 
 def find_max_value(array)
-  # Add your solution here
+counter = 0
+currentmaximum = nil
+while counter < array.size
+  if currentmaximum == nil ||
+    array[counter] > currentmaximum  
+    currentmaximum = array[counter]
+end
+counter += 1
+end
+return currentmaximum
 end
 
+
+# loop through the array
+# for each value, check and see if it's the maximum (biggest) value you've found so far. You only know this if you use a variable to keep track of the current maximum.
+#compare current value to mac and update if you ned to
+
+
 def find_min_value(array)
-  # Add your solution here
+counter = 0
+currentinimum = nil
+while counter < array.size
+  if currentinimum == nil ||
+    array[counter] < currentminimum 
+    currentminimum = array[counter]
 end
+counter += 1
+end
+return currentminimum
+end
+
